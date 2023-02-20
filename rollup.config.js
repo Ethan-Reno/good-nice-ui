1b7f188dd9c9385f2b28a40a7a75f862cbd60990
Ethan Reno <epreno@live.ca>
2023-02-19 22:06:35 -0500

    Init

---
+++ rollup.config.js
@@ -0,0 +1,52 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import peerDeps from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
+import visualizer from 'rollup-plugin-visualizer';
+import { terser } from 'rollup-plugin-terser';
+import { getFiles } from './scripts/buildUtils';
+
+const extensions = ['.js', '.ts', '.jsx', '.tsx'];
+const excludeExtensions = [
+  'test.js',
+  'test.ts',
+  'test.jsx',
+  'test.tsx',
+  'stories.js',
+  'stories.ts',
+  'stories.jsx',
+  'stories.tsx',
+];
+
+export default {
+  input: [
+    './src/index.ts',
+    ...getFiles('./src/common', extensions, excludeExtensions),
+    ...getFiles('./src/components', extensions, excludeExtensions),
+    ...getFiles('./src/hooks', extensions, excludeExtensions),
+  ],
+  output: {
+    dir: 'dist',
+    format: 'esm',
+    preserveModules: true,
+    preserveModulesRoot: 'src',
+    sourcemap: true,
+  },
+  plugins: [
+    peerDeps(),
+    resolve(),
+    commonjs(),
+    typescript({
+      tsconfig: './tsconfig.build.json',
+      declaration: true,
+      declarationDir: 'dist',
+    }),
+    postcss(),
+    terser(),
+    visualizer({
+      filename: 'bundle-analysis.html',
+      open: true,
+    }),
+  ],
+};
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export { Button } from './components/Button';
+export { Input } from './components/Input';
