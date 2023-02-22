e9c93b021b1206417221708468f323d84891eea2
Ethan Reno <epreno@live.ca>
2023-02-22 15:46:00 -0500

    Add theme custimization implementation

---
+++ .storybook/theme.ts
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Lift Companion UI',
+  brandUrl: 'https://example.com',
+  brandTarget: '_self',
+});
