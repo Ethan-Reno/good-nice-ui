899d395d4da547061e273c81d77e24dd68c8b78c
Ethan Reno <epreno@live.ca>
2023-03-02 17:43:39 -0500

    Remove tailwind-merge, update Dialog composition for better readability

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -37,24 +37,22 @@ const DialogContent = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
 >(({ className, children, ...props }, ref) => (
-  <DialogPortal>
-    <DialogOverlay />
-    <DialogPrimitive.Content
-      ref={ref}
-      className={cn(
-        "fixed z-50 grid w-full gap-4 rounded-b-lg bg-white p-6 animate-in data-[state=open]:fade-in-90 data-[state=open]:slide-in-from-bottom-10 sm:max-w-lg sm:rounded-lg sm:zoom-in-90 data-[state=open]:sm:slide-in-from-bottom-0",
-        "dark:bg-zinc-800",
-        className
-      )}
-      {...props}
+  <DialogPrimitive.Content
+    ref={ref}
+    className={cn(
+      "fixed z-50 grid w-full gap-4 rounded-b-lg bg-white p-6 animate-in data-[state=open]:fade-in-90 data-[state=open]:slide-in-from-bottom-10 sm:max-w-lg sm:rounded-lg sm:zoom-in-90 data-[state=open]:sm:slide-in-from-bottom-0",
+      "dark:bg-zinc-800",
+      className
+    )}
+    {...props}
+  >
+    {children}
+    <DialogPrimitive.Close
+      className="absolute top-4 right-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-zinc-400 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-zinc-100 dark:focus:ring-zinc-400 dark:focus:ring-offset-zinc-900 dark:data-[state=open]:bg-zinc-800"
     >
-      {children}
-      <DialogPrimitive.Close className="absolute top-4 right-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-zinc-400 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-zinc-100 dark:focus:ring-zinc-400 dark:focus:ring-offset-zinc-900 dark:data-[state=open]:bg-zinc-800">
-        <XIcon label='Close Dialog' />
-        <span className="sr-only">Close</span>
-      </DialogPrimitive.Close>
-    </DialogPrimitive.Content>
-  </DialogPortal>
+      <XIcon label='Close Dialog' />
+    </DialogPrimitive.Close>
+  </DialogPrimitive.Content>
 ))
 
 const DialogHeader = ({
@@ -127,16 +125,17 @@ const Dialog = ({
 }: DialogProps) => (
   <DialogRoot>
     <DialogTrigger asChild>{trigger}</DialogTrigger>
-    <DialogContent>
+    <DialogPortal>
+    <DialogOverlay />
       <DialogHeader>
         {title && <DialogTitle>{title}</DialogTitle>}
         {description && <DialogDescription>{description}</DialogDescription>}
       </DialogHeader>
-      {content}
+      <DialogContent>{content}</DialogContent>
       <DialogFooter>
         {primaryButton}
       </DialogFooter>
-    </DialogContent>
+    </DialogPortal>
   </DialogRoot>
 );
 
