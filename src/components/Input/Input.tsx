3d215f04463f3af2d06b470ab62e336fa1cb58d7
Ethan Reno <epreno@live.ca>
2023-04-25 20:46:56 -0400

    Add shared focus styling, update input design

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -12,16 +12,15 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
       <div className='relative'>
         <input
           className={clsx(
-            'flex h-10 w-full rounded-md border bg-transparent py-2 px-3 text-sm placeholder:text-zinc-400 disabled:cursor-not-allowed disabled:opacity-50 border-zinc-400',
-            'focus-styles',
+            'block w-full rounded-md bg-transparent border-0 py-1.5 pl-3 pr-20 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 dark:focus:ring-blue-600 sm:text-sm sm:leading-6 focus:outline-none',
             error
-              && 'border-red-500 focus:ring-red-500',
+              && 'ring-red-500 focus:ring-red-500',
             className
           )}
           ref={ref}
           {...props}
         />
-        {typeof(error) === 'string' && <div className="absolute text-xs text-left text-red-400 top-12 right-1">* {error}</div>}
+        {typeof(error) === 'string' && <div className="absolute text-xs text-red-500 top-10 left-1">* {error}</div>}
       </div>
     );
   }
