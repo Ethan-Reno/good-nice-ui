53135b5176cc1b5a556607162560fb137e56f176
Ethan Reno <epreno@live.ca>
2023-05-25 23:42:09 -0400

    Add datepicker example component

---
+++ src/components/DatePicker/DatePicker.tsx
@@ -0,0 +1,40 @@
+import * as React from "react"
+import { format } from "date-fns"
+import { Calendar as CalendarIcon } from "lucide-react"
+import { clsx } from "clsx"
+import { Button } from "../Button/Button"
+import { Calendar } from "../Calendar/Calendar"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "../Popover/Popover";
+
+export function DatePicker() {
+  const [date, setDate] = React.useState<Date>()
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          className={clsx(
+            "w-[280px] justify-start text-left font-normal",
+            !date && "text-muted-foreground"
+          )}
+        >
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {date ? format(date, "PPP") : <span>Pick a date</span>}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0">
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          initialFocus
+        />
+      </PopoverContent>
+    </Popover>
+  )
+}
