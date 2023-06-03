9571c152fbe36ded8c82d3e88370eabc5d3c5369
Ethan Reno <epreno@live.ca>
2023-06-02 22:01:53 -0400

    Add Skeleton loader component

---
+++ src/components/Skeleton/Skeleton.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { clsx } from "clsx";
+
+function Skeleton({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={clsx("animate-pulse rounded-md bg-muted", className)}
+      {...props}
+    />
+  );
+};
+
+export { Skeleton };
