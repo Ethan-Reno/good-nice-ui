b33581e01cbd01d7eb333ebd0c721197beb92bae
Ethan Reno <epreno@live.ca>
2023-06-17 10:08:19 -0400

    Update storybook and plugin versions

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,6 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 const config: StorybookConfig = {
   "stories": [
-    "../src/**/*.mdx",
     "../src/components/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
