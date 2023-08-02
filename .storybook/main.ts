83e2f1bc917c37e51dc13a69ed5e36cdecfd00cc
Ethan Reno <epreno@live.ca>
2023-08-02 00:00:26 -0400

    Update packages and use new styling addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,21 +1,23 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 const config: StorybookConfig = {
-  "stories": [
-    "../src/stories/**/*.mdx",
-    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../src/stories/**/*.mdx',
+    '../src/stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  "addons": [
-    "@storybook/addon-a11y",
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+  addons: [
+    '@storybook/addon-a11y',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-storysource',
+    '@storybook/addon-styling',
   ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
   },
-  "docs": {
-    "autodocs": "tag"
-  }
+  docs: {
+    autodocs: 'tag',
+  },
 };
 export default config;
