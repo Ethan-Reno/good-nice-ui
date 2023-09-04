3ae8125d91621b76369fec2e2a4e295f55456901
Ethan Reno <epreno@live.ca>
2023-09-04 11:18:49 -0400

    Update packages and Card header spacing

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -52,7 +52,7 @@ const Header = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn('flex flex-col space-y-1.5 p-6', className)}
+    className={cn('flex flex-col p-6', className)}
     {...props}
   />
 ));
--- src/stories/components/Card.stories.tsx
+++ src/stories/components/Card.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
-import { Button, Card, Input, Label } from '../../components';
+import { Button, Card, Input, Label, Separator } from '../../components';
 
 const meta = {
   title: 'Components/Card',
@@ -27,17 +27,16 @@ const DefaultCard = () => {
           Include a description for more information.
         </Card.Description>
       </Card.Header>
-      <Card.Content>
-        <div className="flex flex-col space-y-1.5">
-          <Label className="text-lowContrast-foreground" htmlFor="name">
-            Name
-          </Label>
-          <Input
-            className="bg-background"
-            id="name"
-            placeholder="Name of your project"
-          />
-        </div>
+      {/* <Separator /> */}
+      <Card.Content className="flex flex-col">
+        <Label className="text-lowContrast-foreground" htmlFor="name">
+          Name
+        </Label>
+        <Input
+          className="bg-background"
+          id="name"
+          placeholder="Name of your project"
+        />
       </Card.Content>
       <Card.Footer className="flex justify-end gap-4">
         <Button variant="secondary">Cancel</Button>
