a81eb47df92b8be830fa0b876ee4c6bdddaf22ec
Ethan Reno <epreno@live.ca>
2024-04-06 18:49:52 -0400

    Add breadcrumb export

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,6 +4,7 @@ export * from './Alert';
 export * from './AspectRatio';
 export * from './Avatar';
 export * from './Badge';
+export * from './Breadcrumb';
 export * from './Button';
 export * from './Calendar';
 export * from './Card';
